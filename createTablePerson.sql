62c837c95a95d9be7550dde97aeb68302445148c
pauloalchemist <paulolinsdev@gmail.com>
2021-03-28 19:46:42 -0300

    create person

--- createTablePerson.sql
+++ createTablePerson.sql
@@ -2,7 +2,8 @@ CREATE TABLE person (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  gender VARCHAR(5) NOT NULL,
+  gender VARCHAR(15) NOT NULL,
   date_of_birth DATE NOT NULL,
+  country_of_birth VARCHAR(50) NOT NULL,
   email VARCHAR(150) NOT NULL
 );
