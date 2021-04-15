cdd30257296001d426adc8080dbdc0a190699b0c
pauloalchemist <paulolinsdev@gmail.com>
2021-04-14 22:21:32 -0300

    json com postgres

---
+++ order-json.sql
@@ -0,0 +1,11 @@
+drop table orders;
+
+CREATE TABLE orders (
+	id serial NOT NULL PRIMARY KEY,
+	info json NOT NULL
+);
+
+INSERT INTO orders (info)
+VALUES('{ "customer": "Lily Bush", "items": {"product": "Diaper","qty": 24}}'),
+      ('{ "customer": "Josh William", "items": {"product": "Toy Car","qty": 1}}'),
+      ('{ "customer": "Mary Clark", "items": {"product": "Toy Train","qty": 2}}');
