5f75a734add2d8e763cea7ca5a98e71cda55c08a
pauloalchemist <paulolinsdev@gmail.com>
2021-04-14 22:50:29 -0300

    teste

--- order-json.sql
+++ order-json.sql
@@ -9,3 +9,6 @@ INSERT INTO orders (info)
 VALUES('{ "customer": "Lily Bush", "items": {"product": "Diaper","qty": 24}}'),
       ('{ "customer": "Josh William", "items": {"product": "Toy Car","qty": 1}}'),
       ('{ "customer": "Mary Clark", "items": {"product": "Toy Train","qty": 2}}');
+
+//comentário
+
