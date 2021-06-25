72b57331217639eb48c4a8059cf54ac6bf715064
pauloalchemist <paulolinsdev@gmail.com>
2021-06-25 18:39:40 -0300

    criação da tabela conta_corrente

--- postgres_melhores_praticas/tabelas-banco.sql
+++ postgres_melhores_praticas/tabelas-banco.sql
@@ -24,3 +24,15 @@ CREATE TABLE IF NOT EXISTS cliente (
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS conta_corrente (
+    banco_numero INTEGER NOT NULL,
+    agencia_numero INTEGER NOT NULL,
+    numero BIGINT NOT NULL,
+    digito SMALLINT NOT NULL,
+    cliente_numero BIGINT NOT NULL,
+    ativo BOOLEAN NOT NULL DEFAULT TRUE,
+    data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (banco_numero, agencia_numero, numero, digito, cliente_numero),
+    FOREIGN KEY (banco_numero, agencia_numero) REFERENCES agencia (banco_numero, numero),
+    FOREIGN KEY (cliente_numero) REFERENCES cliente (numero)
+);
